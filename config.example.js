c8daf084119eb9066e3ccf71b7f6c1b96bd3fc01
Разбаш Роман Владимирович <R.Razbash@alfacapital.ru>
2026-04-23 21:54:06 +0300

    Add HTML form with config.example.js template

---
+++ config.example.js
@@ -0,0 +1,7 @@
+// Инструкция: 
+// 1. Скопируйте этот файл как config.js
+// 2. Вставьте ваш реальный Azure Function Key
+const CONFIG = {
+    API_URL: 'https://amwealth-preorders-api.azurewebsites.net/api/orders',
+    API_KEY: 'PASTE_YOUR_FUNCTION_KEY_HERE'
+};
